902915b0fba9757361c6ba71457bb9ba2cd10161
lfarrell <farrelldlfarrell@gmail.com>
2018-01-28 17:06:48 -0500

    All maps should work

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import * as d3 from 'd3-jetpack/build/d3v4+jetpack';
 
 d3.queue()
-    .defer(d3.json, 'json_data/final/all_counts.json')
-    .defer(d3.json, 'json_data/final/nc_counts.json')
-    .await(function(error, all, nc) {
+    .defer(d3.json, 'json_data/final_min/all_counts.json')
+    .defer(d3.json, 'json_data/final_min/ny_counts.json')
+    .defer(d3.json, 'json_data/final_min/mwdl_counts.json')
+    .defer(d3.json, 'json_data/final_min/nc_counts.json')
+    .await(function(error, all, esdn, mwdl, nc) {
         let render = () => {
             let width = window.innerWidth - 50;
             let div = d3.select('.tooltip');
 
             const color = (value) => {
+                let colorFormat = (coloring) => {
+                    let c = d3.hsl(coloring);
+
+                    return c + "";
+                };
+
                 let color;
 
                 if (value === 'Not in Copyright') {
-                    color = '#eff3ff';
+                    color = colorFormat('#f4a582');
                 } else if (value === 'In Copyright') {
-                    color = '#bdd7e7';
+                    color = colorFormat('#d1e5f0');
                 } else if (value === 'Creative Commons') {
-                    color = '#6baed6';
+                    color = colorFormat('#f1b6da');
                 } else {
-                    color = '#2171b5';
+                    color = colorFormat('#b8e186');
                 }
 
                 return color;
@@ -92,58 +100,59 @@ d3.queue()
                 nodes.append('text')
                     .attr('dx', 4)
                     .attr('dy', 14)
-                    /*   .tspans(function(d) {
-                           return d3.wordwrap(d.data.rights, 45);  // break line after 15 characters
-                       })*/
                     .text((d) => {
-                        return d3.wordwrap(d.data.rights, 30);
+                        return d.data.rights;
                     });
             };
 
-            let legend = (selector, all) => {
-                let keys = [
-                    'Creative Commons',
-                    'No Known Copyright',
-                    'Copyright Unknown',
-                    'In Copyright'
-                ];
+            let legend = (selector) => {
+                if (! document.querySelectorAll('.legend').length) {
+                    let keys = [
+                        'Creative Commons',
+                        'No Known Copyright',
+                        'Copyright Unknown',
+                        'In Copyright'
+                    ];
 
-                let legend = d3.select(selector)
-                    .append('svg')
-                    .attr('width', width / 2)
-                    .attr('height', 55)
-                    .attr('class', 'legend')
-                    .translate([25, 0]);
+                    let legend = d3.select(selector)
+                        .append('svg')
+                        .attr('width', width / 2)
+                        .attr('height', 55)
+                        .attr('class', 'legend')
+                        .translate([25, 0]);
 
-                let j = 0;
+                    let j = 0;
 
-                legend.selectAll('g').data(keys)
-                    .enter()
-                    .append('g').attr('width', 190)
-                    .each(function (d) {
-                        let g = d3.select(this);
+                    legend.selectAll('g').data(keys)
+                        .enter()
+                        .append('g').attr('width', 190)
+                        .each(function (d) {
+                            let g = d3.select(this);
 
-                        g.append('rect')
-                            .attr('x', j)
-                            .attr('y', 15)
-                            .attr('width', 10)
-                            .attr('height', 10)
-                            .style('fill', color(d));
+                            g.append('rect')
+                                .attr('x', j)
+                                .attr('y', 15)
+                                .attr('width', 10)
+                                .attr('height', 10)
+                                .style('fill', color(d));
 
-                        g.append('text')
-                            .attr('x', j + 15)
-                            .attr('y', 25)
-                            .attr('height', 30)
-                            .attr('width', d.length * 50)
-                            .style('font-size', '0.75em')
-                            .text(d);
+                            g.append('text')
+                                .attr('x', j + 15)
+                                .attr('y', 25)
+                                .attr('height', 30)
+                                .attr('width', d.length * 50)
+                                .style('font-size', '0.75em')
+                                .text(d);
 
-                        j += (d.length * 5) + 50;
-                    });
+                            j += (d.length * 5) + 50;
+                        });
+                }
             };
 
             legend('#all-legend');
             mapped(all, '#all', 700);
+            mapped(esdn, '#esdn', 500);
+            mapped(mwdl, '#mwdl', 500);
             mapped(nc, '#nc', 500);
 
             d3.selectAll('.row').classed('hide', false);
